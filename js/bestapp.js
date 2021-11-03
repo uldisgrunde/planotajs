6b961ad843ec9a891daf815e9885984f39501b2b
Uldis Grunde-Zeiferts <uldis.grunde@gmail.com>
2021-11-03 15:31:57 +0200

    Sandis_Karlis_Kristians_Oskars

---
+++ js/bestapp.js
@@ -0,0 +1,9 @@
+function GetJob(){}
+	for(let i=1 ;i<2;i++){
+		let html=`\t\t\t<h2 class="activity-main">
+		${activity[i]["title"]}<br>
+		${activity[i]["start_date"]}<br>
+		${activity[i]["end_date"]}`
+		//if (i > 2) exit;
+	}
+}
