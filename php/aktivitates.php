ec4d9e102c1ed8f4be36b81c82f6c705d0a9d800
Jurmaize <90753623+Jurmaize@users.noreply.github.com>
2021-11-03 22:28:18 +0200

    Commit #6
    Pārveidoju php failus

--- php/aktivitates.php
+++ php/aktivitates.php
@@ -1,33 +1,47 @@
-<!DOCTYPE html>
 <html lang="lv">
-<?php include 'connect.php';
+<?php
+$servername='auth-db150.hostinger.com';
+$username='u353443769_beis';
+$dbname = 'u353443769_beis';
+$password="testadmin";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn){
+    die('Could not Connect My Sql:' .mysql_error());
+}
 ?>
 <head>
     <meta charset="UTF-8">
     <title>Aktivitates</title>
-
-    <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="./../css/style.css">
 </head>
+<?php
+include 'process.php';
+// mysql apstrade
+?>
 <body>
 <div class="container">
-    <form action="aktivitates.html" id="aktform">
-        Nosaukums <input type="text" name="name">
-        <input type="submit">
-    </form>
-    <br>
-    <textarea rows="4" cols="50" name="comment" form="aktform">
-Ievadiet aktivitati</textarea>
+    <form method="post" action="./process.php">
+        <p>
+            <label for="nosaukums">Nosaukums:</label>
+            <input type="text" name="nosaukums">
+        </p>
+        <p>
+            <label for="darbiba">Darbiba:</label>
+            <input type="text" name="darbiba">
+        </p>
 
-    <label for="start">Start date:</label>
-    <input class="input-date" type="date" id="start" name="akt-sakums"
+    <label for="sakums">Start date:</label>
+    <input class="input-date" type="date" name="sakums"
            value="YYYY-MM-DD"
            min="2021-10-26" max="2022-12-31">
 
     <label for="beigas">End date:</label>
-    <input class="input-date" type="date" id="beigas" name="akt-beigas"
+    <input class="input-date" type="date" name="beigas"
            value="YYYY-MM-DD"
            min="2021-10-26" max="2022-12-31">
+</div>
+<div style="text-align:center;">
+    <input type="submit" name="save" value="Iesniegt">
 </div>
 </body>
 </html>
