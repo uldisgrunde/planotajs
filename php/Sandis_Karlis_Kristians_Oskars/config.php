1a48cb8dbf506353ad7df4dc29f5449c1653a950
S-Smans <58655856+S-Smans@users.noreply.github.com>
2022-06-08 15:57:36 +0300

    Create config.php

---
+++ php/Sandis_Karlis_Kristians_Oskars/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbname = "u353443769_beis";
+$servername = "auth-db150.hostinger.com";
+$username = "u353443769_beis";
+$password = "testadmin";
+$connection = mysqli_connect($servername, $username, $password, $dbname)
+?>
