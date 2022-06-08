15253f1a90ca9b65f34bea31ca99178547d6805f
S-Smans <58655856+S-Smans@users.noreply.github.com>
2022-06-08 16:02:08 +0300

    Delete config.php

--- php/Sandis_Karlis_Kristians_Oskars/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbname = "u353443769_beis";
-$servername = "auth-db150.hostinger.com";
-$username = "u353443769_beis";
-$password = "testadmin";
-$connection = mysqli_connect($servername, $username, $password, $dbname)
-?>
